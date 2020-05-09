d2dbe023ed99270691249ac052f912931538f46c
slalonde00 <slalonde00@gmail.com>
2020-05-09 00:29:48 -0400

    skeleton of the program

--- voting-boot/src/App.js
+++ voting-boot/src/App.js
@@ -4,20 +4,54 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from "react-bootstrap/Button";
 import ReactDOM from 'react-dom';
 import Jumbotron from "react-bootstrap/Jumbotron";
- 
-function App() {
- 
+import  './Candidat.js';
 
-    const [value, setValue] = useState('');
+ function   App() {
 
-    const handleChange = (event) => setValue(event.target.value);
-    const handleSubmit = (event) => {
     
 
-        alert('Un candidat a été ajouter à la course aux votes: ' + value);
+    const [value, setValue] = useState();
 
+    
+    const [Candidat, setCandidat] = useState();
+
+
+    const [Candidat1, setCandidat1] = useState();
+    
+    const [Candidat2, setCandidat2] = useState();
+
+    const [Candidat3, setCandidat3] = useState();
+
+        const list = [Candidat];
+ 
+    const SimpleList = () => (
+        <ul>
+          {list.map(item => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+      );
+
+    const handleChange = (event) => setValue(event.target.value);
+    
+    const handleClick = (event) =>{
+        
+        setCandidat(value);
+        Candidat = new Candidat(value);
+        list.unshift(Candidat);
+
+        
+        
+    
+    }
+
+    const handleSubmit = (event) => {
+        alert('Un candidat a été ajouter à la course aux votes: ' + value);
+        alert('La  Liste de Candidat contient : ' + list.toString); //Méthode toString à redéfinir pour afficher la liste des Candidats
+        
         event.preventDefault();
     }
+  
 
     return (
         <form onSubmit={handleSubmit}>
@@ -25,7 +59,17 @@ function App() {
                 Name:
                 <input type="text" value={value} onChange={handleChange} />
             </label>
-            <input type="submit" value="Submit" />
-        </form>
+            <br></br>
+            <input onClick={handleClick} type="submit" value="Submit" />
+            
+            <Jumbotron>
+            {SimpleList()}
+            </Jumbotron>
+
+                    </form>
+            
     );
-}export default App;
+
+
+    }
+export default App;
---
+++ voting-boot/src/Candidat.js
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react';
+
+class Candidat extends React.Component {
+
+    constructor(props) {
+        super(props);
+   this.state = {value: ''}
+      }
+
+      
+    }
