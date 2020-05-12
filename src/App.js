ecf81e595c93be00e1da18698eb4ea4afaab248f
koffi79 <63424663+koffi79@users.noreply.github.com>
2020-05-12 18:36:19 -0400

    creation3
    Co-Authored-By: Sebast <sebast@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from "react-bootstrap/Button";
-import ReactDOM from 'react-dom';
 import Jumbotron from "react-bootstrap/Jumbotron";
 
 function Candidat(props) {
@@ -21,29 +19,29 @@ function Candidat(props) {
   };
 
   return (
-    <div class="row">
-      <div class="col-md-12">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" 
+    <div className="row">
+      <div className="col-md-12">
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" 
           placeholder={"Ajouter un  Candidat"} value={props.val}
           onChange={handleChange}/>
-          <div class="input-group-append">
-            <button type="button" class="btn btn-primary" onClick={handleAdd}>
+          <div className="input-group-append">
+            <button type="button" className="btn btn-primary" onClick={handleAdd}>
               Ajouter
             </button>
           </div>
           
         </div>  
       </div>
-      <div class="col-md-12">
-        <button type="button" class="btn btn-success" onClick={handleNotreComponent}>
+      <div className="col-md-12">
+        <button type="button" className="btn btn-success" onClick={handleNotreComponent}>
           Démarrer le vote
         </button>
       </div>
-      <div class="col-md-12">
+      <div className="col-md-12">
         <hr/>
         </div>
-        <div class="col-md-12 text-secondary">
+        <div className="col-md-12 text-secondary">
           <Jumbotron>
             <ul>{props.list && props.list.map((listItem, i) => <li key={i}>{listItem}</li>)}</ul>
           </Jumbotron>
@@ -53,27 +51,30 @@ function Candidat(props) {
 }
 
 function Vote(props) {
+  const handleVote = () => {
+    props.vote("vote");
+  };
 
   return (
-    <div class="row">     
-      <div class="col-md-12">
+    <div className="row">     
+      <div className="col-md-12">
         <button type="button" class="btn btn-danger">
           Arrêter le vote
         </button>
       </div>
-      <div class="col-md-12">
+      <div className="col-md-12">
         <hr/>
         </div>
-        <div class="col-md-12 text-secondary">
+        <div className="col-md-12 text-secondary">
           <Jumbotron>
             <ul>
               {props.list && props.list.map((listItem, i) => <li key={i}>
-                <div class="row">
-                  <div class="col-md-9">
+                <div className="row">
+                  <div className="col-md-9">
                     {listItem}
                   </div>
-                  <div class="col-md-3">
-                    <button type="button" class="btn btn-primary">
+                  <div className="col-md-3">
+                    <button type="button" className="btn btn-primary" onClick={handleVote}>
                       Voter
                     </button>
                   </div>
@@ -102,7 +103,7 @@ function Gagnant(props) {
 }
 
 function App() {
-    const [list, setList] = useState([]);
+    const [listCandidat, setListCandidat] = useState([]);
     const [val, setVal] = useState("");
     //La variable temoin peut prendre les valeurs possibles suivantes :
     //candidat | vote | gagnant
@@ -113,15 +114,15 @@ function App() {
       <div className="App">
         <header className="App-header">
           
-          {varComponent == "candidat"
-            ? <Candidat list={list} val={val} setList={setList} 
+          {
+            varComponent == "candidat"
+            ? <Candidat list={listCandidat} val={val} setList={setListCandidat} 
                         setVal={setVal} notreComponent={notreComponent}/>
             : 
             (varComponent == "vote"
-            ? <Vote list={list} notreComponent={notreComponent} />
+            ? <Vote list={listCandidat} notreComponent={notreComponent} />
             : <Gagnant />
-            )
-            
+            )            
           }
         </header>
         <main className="App-main">
