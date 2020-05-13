b0acc8990112af12c0415745a54a45d7583f2df3
koffi79 <63424663+koffi79@users.noreply.github.com>
2020-05-12 20:06:10 -0400

    Update App.js
    Co-Authored-By: Sebast <sebast@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -10,11 +10,18 @@ function Candidat(props) {
   };
 
   const handleAdd = () => {
-    props.setList([...props.list, props.val]);
+    props.setList([
+      ...props.list, //liste de candidats
+      [ //tablea double entrée qui renseigne candidat et vote (0 par défaut))
+        props.val,
+        0 //Vote 0 par défaut
+      ]
+    ]);
     props.setVal("");
   };
 
   const handleNotreComponent = () => {
+    //Permet de changer le component pour vote en passant la varaible témoin
     props.notreComponent("vote");
   };
 
@@ -43,7 +50,11 @@ function Candidat(props) {
         </div>
         <div className="col-md-12 text-secondary">
           <Jumbotron>
-            <ul>{props.list && props.list.map((listItem, i) => <li key={i}>{listItem}</li>)}</ul>
+          <ul>
+              {props.list && props.list.map((listItem, i) => <li key={i}>
+                  {listItem[0]}
+              </li>)}
+            </ul>
           </Jumbotron>
         </div>
       </div>
@@ -51,14 +62,41 @@ function Candidat(props) {
 }
 
 function Vote(props) {
-  const handleVote = () => {
-    props.vote("vote");
+
+  console.log(props.list);
+  const handleVote = e => {
+    console.log(e.target.id);
+    for(var i = 0; i < props.list.length; i++){
+      var element = props.list[i];
+      var nomCandidat = element[0];
+      //Ça veut dire que le bouton voter cliqué est pour ce candidat
+      if(nomCandidat == e.target.id){
+        var vote = element[1];
+        vote += 1;
+        props.list[i][1] = vote;  //mettre à jour le vote
+        break;
+      }
+    }
+    //console.log(props.list);
+  };  
+
+  const handleNotreComponent = () => {
+    //Algorithme du gagnant
+    var listeCandidats = props.list.slice(0); //Prendre une copie de l'autre sans garder la référence
+    listeCandidats.sort((a,b) => a[1] - b[1]);  //Ordre croissant
+    listeCandidats.reverse()  //Ordre décroissant
+    console.log(listeCandidats);
+    //Mettre à jour le gagnant
+    var nomGagnant = listeCandidats[0][0];
+    props.setGagant(nomGagnant);
+    //Permet de changer le component pour gagant en passant la varaible témoin
+    props.notreComponent("gagnant");
   };
 
   return (
     <div className="row">     
       <div className="col-md-12">
-        <button type="button" class="btn btn-danger">
+        <button type="button" className="btn btn-danger" onClick={handleNotreComponent}>
           Arrêter le vote
         </button>
       </div>
@@ -71,10 +109,12 @@ function Vote(props) {
               {props.list && props.list.map((listItem, i) => <li key={i}>
                 <div className="row">
                   <div className="col-md-9">
-                    {listItem}
+                    {listItem[0]}
                   </div>
                   <div className="col-md-3">
-                    <button type="button" className="btn btn-primary" onClick={handleVote}>
+                    <button type="button" className="btn btn-primary" 
+                      id={listItem[0]}
+                      onClick={handleVote}>
                       Voter
                     </button>
                   </div>
@@ -88,17 +128,33 @@ function Vote(props) {
 }
 
 function Gagnant(props) {
+  //console.log(props.list);
 
   return (
-    <div class="row">     
-      <div class="col-md-12">
-        <hr/>
-        </div>
-        <div class="col-md-12 text-secondary">
-          <Jumbotron>
+    <div className="row">     
+      <div className="col-md-12 text-secondary">
+        <Jumbotron>
+          <h4>=== GAGNANT ===</h4>
+          {props.varGagnant}
+        </Jumbotron>
+      </div>
+      <div className="col-md-12 text-secondary">
+        <Jumbotron>
+            <ul>
+              {props.list && props.list.map((listItem, i) => <li key={i}>
+                <div className="row">
+                  <div className="col-md-9">
+                    {listItem[0]}
+                  </div>
+                  <div className="col-md-3">
+                  {listItem[1]}
+                  </div>
+                </div>
+              </li>)}
+            </ul>
           </Jumbotron>
         </div>
-      </div>
+    </div>
   );
 }
 
@@ -109,6 +165,7 @@ function App() {
     //candidat | vote | gagnant
     //Chacune de ces valeurs permet de rendre un component
     const [varComponent, notreComponent] = useState("candidat");
+    const [varGagnant, setGagant] = useState("");
   
     return (
       <div className="App">
@@ -120,8 +177,10 @@ function App() {
                         setVal={setVal} notreComponent={notreComponent}/>
             : 
             (varComponent == "vote"
-            ? <Vote list={listCandidat} notreComponent={notreComponent} />
-            : <Gagnant />
+            ? <Vote list={listCandidat} notreComponent={notreComponent} 
+              setGagant={setGagant} />
+            : <Gagnant list={listCandidat} notreComponent={notreComponent} 
+                varGagnant={varGagnant} />
             )            
           }
         </header>
